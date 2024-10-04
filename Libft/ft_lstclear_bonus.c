6151365dcfe1b8165e894c6015c3c9b5eb0a878b
proton <b.proton@hotmail.com>
2024-10-04 11:05:13 +0200

    just the beginning

---
+++ Libft/ft_lstclear_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bproton <bproton@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 16:44:33 by bproton           #+#    #+#             */
+/*   Updated: 2023/11/08 13:58:41 by bproton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = NULL;
+		*lst = temp;
+	}
+	free(*lst);
+	*lst = NULL;
+}
