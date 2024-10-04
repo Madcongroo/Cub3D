29b6f0dd007b21cfecab3002ead40d12c13505dd
proton <b.proton@hotmail.com>
2024-10-04 18:03:14 +0200

    just some headers

--- Include/cub3d.h
+++ Include/cub3d.h
@@ -11,3 +11,18 @@
 //
 //
 
+# ifndef CUB3D_H
+# define CUB3D_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include <limits.h>
+
+
+
+# endif
