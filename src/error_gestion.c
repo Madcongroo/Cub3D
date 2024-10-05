911187b5268e10b7a76d6ef4f9a88723e4e5de0e
proton <b.proton@hotmail.com>
2024-10-05 14:14:33 +0200

    just adjustig error gestion

--- src/error_gestion.c
+++ src/error_gestion.c
@@ -22,12 +22,18 @@ static void	free_struct_player(t_player *player)
 
 static void	free_struct_textures(t_textures *text)
 {
-	free (text->ce);
-	free (text->ea);
-	free (text->fl);
-	free (text->no);
-	free (text->so);
-	free (text->we);
+	if (text->ce)
+		free (text->ce);
+	if (text->ce)
+		free (text->ea);
+	if (text->fl)
+		free (text->fl);
+	if (text->no)
+		free (text->no);
+	if (text->so)
+		free (text->so);
+	if (text->we)
+		free (text->we);
 }
 
 void	ft_free_array(char **array)
@@ -48,9 +54,9 @@ int	free_all(t_data *data, char *str)
 	if (data)
 	{
 		ft_free_array(data->map);
-		free_strcut_player(data->player);
+		free_struct_player(data->player);
 		free_struct_textures(data->text);
 	}
-	ft_pustr_fd(str, 2);
+	ft_putstr_fd(str, 2);
 	return (1);
 }
