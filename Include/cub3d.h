d4ae2d1ad4c600bb50b988504c9aeafeaa21f0c2
tmartin2 <tmartin2@student.42lausanne.ch>
2024-10-07 15:47:33 +0200

    structure

--- Include/cub3d.h
+++ Include/cub3d.h
@@ -24,5 +24,57 @@
 #include <limits.h>
 
 
+typedef struct s_game
+{
+    t_player *player;
+    t_map *map;
+    t_raycast *raycast;
+    void *win; // Pointeur sur la fenetre mlx
+    void *text; // Pointeur pour les texture du jeux 
+}   t_game;
+
+typedef struct s_player
+{
+    int x;
+    int y;
+    int x_cam;
+    int y_cam;
+    int speed;
+    int plan_x;
+    int plan_y;
+}   t_player;
+
+typedef struct s_rgb 
+{
+    int r;
+    int g;
+    int b;
+} t_rgb;
+
+typedef struct s_map
+{
+    char **map_array;
+    int width;
+    int height;
+    char *n_texture;
+    char *s_texture;
+    char *w_texture;
+    char *e_texture;
+    t_rgb floor_color;
+    t_rgb ceilling_color;
+    int fd;
+}   t_map;
+
+typedef struct s_raycast
+{
+    bool touch_wall;
+    double dist_x;
+    double dist_y;
+    int result;
+    double side_dist_x;
+    double side_dist_y;
+    int step_x;
+    int step_y; 
+}   t_raycast;
 
 # endif
