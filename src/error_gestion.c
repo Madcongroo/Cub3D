4dd1adf3d99daaf13ba3ef3ebbda53f9dd83e70f
Tom Martinelli <tmartin2@c1r4s4.42lausanne.ch>
2024-10-09 16:10:05 +0200

    push tetst

--- src/error_gestion.c
+++ src/error_gestion.c
@@ -20,7 +20,7 @@ static void	free_struct_player(t_player *player)
 	(void)player;
 }
 
-static void	free_struct_textures(t_map *map)
+static void	free_struct_map(t_map *map)
 {
 	if (map->no)
 		free (map->no);
@@ -30,6 +30,8 @@ static void	free_struct_textures(t_map *map)
 		free (map->so);
 	if (map->we)
 		free (map->we);
+	if (map->map_array)
+		ft_free_array(map->map_array);
 }
 
 void	ft_free_array(char **array)
@@ -49,11 +51,12 @@ int	free_all(t_data *data, char *str)
 {
 	if (data)
 	{
-		if (data->map->map_array)
-			ft_free_array(data->map->map_array);
-		free_struct_player(data->player);
-		free_struct_textures(data->text);
+		if (data->map)
+			free_struct_map(data->map);
+		if (data->player)
+			free_struct_player(data->player);
 	}
 	ft_putstr_fd(str, 2);
 	return (1);
 }
+
