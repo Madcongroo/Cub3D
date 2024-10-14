b44a29e051a494d006b0ffca3dd54396da421454
proton <b.proton@hotmail.com>
2024-10-14 15:32:30 +0200

    test 2 merge

--- src/parsing/start_parsing.c
+++ src/parsing/start_parsing.c
@@ -83,6 +83,11 @@ static char	**split_buff(char *buf)
 {
 	char	**map_array;
 
+	if (check_buffer(buf) == -1)
+	{
+		free (buf);
+		return (NULL);
+	}
 	map_array = ft_split(buf, '\n');
 	if (!map_array)
 	{
@@ -116,37 +121,6 @@ static char	**read_map(char *file)
 	else if (check == 0)
 		buf[check] = '\0';
 	return (split_buff(buf));
-}
-
-// int	fill_struct(t_data *data, char **map)
-// {
-// }
-
-/*checks if all the textures are on the map.
-	If the map is at the bottom of the file / is on the file*/
-int	first_map_check(char **array)
-{
-	int	i;
-	int	textures_mark;
-	int	map_mark;
-
-	textures_mark = 0;
-	map_mark = 0;
-	i = -1;
-	while (array[++i])
-	{
-		if ((map_mark == 1 && textures_mark != 6) || textures_mark > 6)
-			return (-1);
-		if ((array[i][0] == 'N' || array[i][0] == 'S' || array[i][0] == 'E'
-			|| array[i][0] == 'W' || array[i][0] == 'C' || array[i][0] == 'F')
-				&& map_mark == 0)
-			textures_mark++;
-		if (array[i][0] == '0' || array[i][0] == '1')
-			map_mark = 1;
-	}
-	if (textures_mark < 6 || !map_mark)
-		return (-1);
-	return (0);
 }
 
 int	start_parsing(t_data *data, char *file)
